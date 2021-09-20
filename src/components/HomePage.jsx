97db4dbd88a438998076355de7eb27a2d65e9b48
Richard Miller <richardmmill@gmail.com>
2021-09-20 15:41:05 -0400

    stats on homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,17 +11,20 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 const HomePage = () => {
     const { data, error, isFetching } = useGetCryptosQuery();
     console.log('data:', data);
-    console.log('error:', error);
+    // console.log('error:', error);
+    const globalStats = data?.data?.stats;
+
+    if(isFetching) return <div>Loading...</div>
 
     return (
         <>
             <Typography.Title level={2}>Global Crypto Statistics</Typography.Title>   
             <Row>
-                <Col span={12}><Statistic title='Total Cryptocurrencies' value='5'></Statistic></Col>
-                <Col span={12}><Statistic title='Total Exchanges' value='5'></Statistic></Col>
-                <Col span={12}><Statistic title='Total Market Cap' value='5'></Statistic></Col>
-                <Col span={12}><Statistic title='Total 24 Hour Volume' value='5'></Statistic></Col>
-                <Col span={12}><Statistic title='Total Markets' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total Cryptocurrencies' value={globalStats.total}></Statistic></Col>
+                <Col span={12}><Statistic title='Total Exchanges' value={globalStats.totalExchanges}></Statistic></Col>
+                <Col span={12}><Statistic title='Total Market Cap' value={globalStats.totalMarketCap}></Statistic></Col>
+                <Col span={12}><Statistic title='Total 24 Hour Volume' value={globalStats.total24hVolume}></Statistic></Col>
+                <Col span={12}><Statistic title='Total Markets' value={globalStats.totalMarkets}></Statistic></Col>
             </Row>
         </>
     )
